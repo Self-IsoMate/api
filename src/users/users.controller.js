11f81e7941bd809917f03cd5024250d6f87c219a
sophie-norman <s.norman2@newcastle.ac.uk>
2020-03-29 10:09:48 +0100

    adding quick fix from master

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,22 +1,8 @@
-/*
-
-	The point of this class is to separate the mongoose/mongodb methods and functions from
-	the routing/endpoint logic that is in main.js
-
-	endpoints to do:
-	- delete user
-	- update user
-	- get user by id
-	- get user by username
-	- get all the users
-	- get all the users by a bunch of filters
-
-*/
-
 var mongoose   = require('mongoose');
 var schema = require('./users.model');
 var cors = require('cors');
 var userSchema = require('./users.model');
+const User = mongoose.model('user', userSchema, 'user_registration'); //export the model
 
 const UserController = {
 	addUser: async (request, response) => {
