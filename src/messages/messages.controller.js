92ed40ec969875e4198adec8aae260caf1eb4a77
Helen Evans <helenijevans@gmail.com>
2020-06-17 21:34:30 +0100

    test

--- src/messages/messages.controller.js
+++ src/messages/messages.controller.js
@@ -68,7 +68,7 @@ const messagesController = {
 
 			   
 		getMessage: async (request, response) => {
-			Message.findById(request.params.message_id, (err, res) => {
+			Message.findById(request.params.message_id.limit(25), (err, res) => {
 					if (err)
 						response.send(err);
 					
